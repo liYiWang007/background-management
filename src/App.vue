049ceb225c506c521122fb01318d36a0ee7f56b0
LiYiWang007 <429521830@qq.com>
2021-02-26 01:16:22 +0800

    chore: 独立无根分支vuex练习

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <product-list-one :products="products"></product-list-one>
+    <product-list-two :products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductListOne,
+    ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        { name: "马云", price: 200 },
+        { name: "马冬梅", price: 100 },
+        { name: "马化腾", price: 40 },
+        { name: "沈腾", price: 10 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+.radius-4 {
+  border-radius: 4px;
+}
+.blog-card {
+  width: 100%;
+  height: auto;
+  padding: 20px;
+  background: #f8f8f8;
+  border: 1px dashed #ddd;
+  margin-top: 20px;
+  box-sizing: border-box;
+}
+.margin-a {
+  margin: 0 auto;
+}
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="product-list-one">
+        <h2>Product List One</h2>
+        <ul>
+            <li v-for="(item, index) in products" :key="index">
+                <span class="name">{{ item.name }}</span>
+                <span class="price"> ${{ item.price }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProductListOne",
+  props: ["products"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style scoped>
+.product-list-one {
+  width: 300px;
+  padding: 20px;
+  background: #f3db87;
+  margin-bottom: 20px;
+}
+.product-list-one h2 {
+  color: #ffffff;
+}
+.product-list-one li {
+  display: inline-block;
+  padding: 10px;
+  margin: 6px;
+  background: #fff6d5;
+}
+.name {
+  color: #4a3b05;
+}
+.price {
+  font-weight: bold;
+  color: #f38787;
+}
+</style>
+
+
